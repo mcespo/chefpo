3cba39889020352b9d7ed0f0054a09325da81834
Marvin Céspedes <mcespo@gmail.com>
2024-09-05 22:29:15 -0500

    prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
+import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chefpo.com',
-  integrations: [mdx(), sitemap(), tailwind(), alpinejs()]
+  integrations: [mdx(), sitemap(), tailwind(), alpinejs()],
+  output: 'server',
+  adapter: netlify(),
 });
