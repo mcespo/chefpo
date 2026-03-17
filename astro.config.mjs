3e83c22912934f32ee7f91e1113970f24fbb1731
Marvin Cespedes <mcespo@gmail.com>
2026-03-16 19:24:53 -0500

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Chefpo",
-      social: {
-        github: "https://github.com/withastro/starlight",
-      },
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/withastro/starlight" },
+      ],
       sidebar: [
         {
           label: "Meals",
