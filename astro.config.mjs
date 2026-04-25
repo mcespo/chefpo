060dea12887d51b08ab4b05631f575cc3c714d17
Marvin Céspedes <mcespo@gmail.com>
2026-04-24 22:58:58 -0500

    startlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightThemeVintage from "starlight-theme-vintage";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
+      plugins: [starlightThemeVintage()],
       title: "Chefpo",
       social: [
         { icon: "github", label: "GitHub", href: "https://github.com/withastro/starlight" },
